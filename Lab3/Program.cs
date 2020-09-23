ad23cb26e825de2895be481e6f6432f03c42429e
YuliaKova <62650948+YuliaKova@users.noreply.github.com>
2020-09-23 20:00:00 +0300

    Add files via upload

---
+++ Lab3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3526;
+
+            int b = a/100;
+            int c = a % 100;
+
+            if ((b/10)+(b%10)==(c/10)+(c%10))
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
