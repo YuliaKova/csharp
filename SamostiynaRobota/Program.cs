d2a4d4c7406a0d47feb5acfb489664621d456e6e
YuliaKova <62650948+YuliaKova@users.noreply.github.com>
2020-11-11 12:52:14 +0200

    Add files via upload

---
+++ SamostiynaRobota/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SamostiynaRobota
+{
+    class Program
+    {
+        static void Main(string[] args) //Варіант 10
+        {
+            Teachers Petryuk = new Teachers("Maria Petrivna", 50, 27.0, "Math", false);
+            Teachers Semanyuk = new Teachers("Petro Semenovich", 45, 24.0, "English", true);
+            Teachers Ivanyuk = new Teachers("Semen Ivanovich", 40, 15.0, "Math", true);
+            Teachers Vasylyuk = new Teachers("Ivan Vasylyovich", 35, 15.1, "Phisics", false);
+            Teachers Igoryuk = new Teachers("Vasylisa Igorivna", 40, 15.0, "Programming", true);
+
+            Teachers.AddTeacher(Petryuk);
+            Teachers.AddTeacher(Semanyuk);
+            Teachers.AddTeacher(Ivanyuk);
+            Teachers.AddTeacher(Vasylyuk);
+            Teachers.AddTeacher(Igoryuk);
+
+            Teachers.ChooseTeacher(50, 27.0, "Math", false);
+            Teachers.ChooseTeacher(45, 24.0, "English");
+            Teachers.ChooseTeacher(40, 15.0, true);
+            Teachers.ChooseTeacher(50, "Math", true);
+            Teachers.ChooseTeacher(15.1, "Math", true);
+            Teachers.ChooseTeacher(40, 15.0);
+            Teachers.ChooseTeacher(35, "English");
+            Teachers.ChooseTeacher(40, true);
+            Teachers.ChooseTeacher(24.0, "Phisics");
+            Teachers.ChooseTeacher(15.1, false);
+            Teachers.ChooseTeacher("Programming", true);
+            Teachers.ChooseTeacher(45);
+            Teachers.ChooseTeacher(15.0);
+            Teachers.ChooseTeacher("English");
+            Teachers.ChooseTeacher(true);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SamostiynaRobota/Teachers.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SamostiynaRobota
+{
+    class Teachers
+    {
+        string name;
+        int age;
+        double experience;
+        string discipline;
+        bool englishKnowledge;
+
+        static List<Teachers> listOfTeachers = new List<Teachers>();
+        public Teachers(string name, int age, double experience, string discipline, bool englishKnowledge)
+        {
+            this.name = name;
+            this.age = age;
+            this.experience = experience;
+            this.discipline = discipline;
+            this.englishKnowledge = englishKnowledge;
+        }
+        static public void AddTeacher(Teachers Obj)
+        {
+            listOfTeachers.Add(Obj);
+        }
+        public void GetTeachersInfo()
+        {
+            Console.WriteLine("\nName: " + name + "\nAge: " + age + "\nExperience: " + experience + "\nDiscipline: " + discipline + "\nEnglish Knowledge: " + englishKnowledge);
+        }
+        static public void ChooseTeacher(int age, double experience, string discipline, bool englishKnowledge)
+        {
+            Console.WriteLine("\nResult: ");
+
+            bool notFound = true;
+            foreach (Teachers i in listOfTeachers)
+            {
+                if (i.age == age && i.experience == experience && i.discipline == discipline && i.englishKnowledge == englishKnowledge)
+                {
+                    i.GetTeachersInfo();
+                    notFound = false;
+                }
+            }
+            if (notFound) Console.WriteLine("\n\tNot Found");
+        }
+        static public void ChooseTeacher(int age, double experience, string discipline)
+        {
+            Console.WriteLine("\nResult: ");
+
+            bool notFound = true;
+            foreach (Teachers i in listOfTeachers)
+            {
+                if (i.age == age && i.experience == experience && i.discipline == discipline)
+                {
+                    i.GetTeachersInfo();
+                    notFound = false;
+                }
+            }
+            if (notFound) Console.WriteLine("\n\tNot Found");
+        }
+        static public void ChooseTeacher(int age, double experience, bool englishKnowledge)
+        {
+            Console.WriteLine("\nResult: ");
+
+            bool notFound = true;
+            foreach (Teachers i in listOfTeachers)
+            {
+                if (i.age == age && i.experience == experience && i.englishKnowledge == englishKnowledge)
+                {
+                    i.GetTeachersInfo();
+                    notFound = false;
+                }
+            }
+            if (notFound) Console.WriteLine("\n\tNot Found");
+        }
+        static public void ChooseTeacher(int age, string discipline, bool englishKnowledge)
+        {
+            Console.WriteLine("\nResult: ");
+
+            bool notFound = true;
+            foreach (Teachers i in listOfTeachers)
+            {
+                if (i.age == age && i.discipline == discipline && i.englishKnowledge == englishKnowledge)
+                {
+                    i.GetTeachersInfo();
+                    notFound = false;
+                }
+            }
+            if (notFound) Console.WriteLine("\n\tNot Found");
+        }
+        static public void ChooseTeacher(double experience, string discipline, bool englishKnowledge)
+        {
+            Console.WriteLine("\nResult: ");
+
+            bool notFound = true;
+            foreach (Teachers i in listOfTeachers)
+            {
+                if (i.experience == experience && i.discipline == discipline && i.englishKnowledge == englishKnowledge)
+                {
+                    i.GetTeachersInfo();
+                    notFound = false;
+                }
+            }
+            if (notFound) Console.WriteLine("\n\tNot Found");
+        }
+        static public void ChooseTeacher(int age, double experience)
+        {
+            Console.WriteLine("\nResult: ");
+
+            bool notFound = true;
+            foreach (Teachers i in listOfTeachers)
+            {
+                if (i.age == age && i.experience == experience)
+                {
+                    i.GetTeachersInfo();
+                    notFound = false;
+                }
+            }
+            if (notFound) Console.WriteLine("\n\tNot Found");
+        }
+        static public void ChooseTeacher(int age, string discipline)
+        {
+            Console.WriteLine("\nResult: ");
+
+            bool notFound = true;
+            foreach (Teachers i in listOfTeachers)
+            {
+                if (i.age == age && i.discipline == discipline)
+                {
+                    i.GetTeachersInfo();
+                    notFound = false;
+                }
+            }
+            if (notFound) Console.WriteLine("\n\tNot Found");
+        }
+        static public void ChooseTeacher(int age, bool englishKnowledge)
+        {
+            Console.WriteLine("\nResult: ");
+
+            bool notFound = true;
+            foreach (Teachers i in listOfTeachers)
+            {
+                if (i.age == age && i.englishKnowledge == englishKnowledge)
+                {
+                    i.GetTeachersInfo();
+                    notFound = false;
+                }
+            }
+            if (notFound) Console.WriteLine("\n\tNot Found");
+        }
+        static public void ChooseTeacher(double experience, string discipline)
+        {
+            Console.WriteLine("\nResult: ");
+
+            bool notFound = true;
+            foreach (Teachers i in listOfTeachers)
+            {
+                if (i.experience == experience && i.discipline == discipline)
+                {
+                    i.GetTeachersInfo();
+                    notFound = false;
+                }
+            }
+            if (notFound) Console.WriteLine("\n\tNot Found");
+        }
+        static public void ChooseTeacher(double experience, bool englishKnowledge)
+        {            
+            Console.WriteLine("\nResult: ");
+
+            bool notFound = true;
+            foreach (Teachers i in listOfTeachers)
+            {
+                if (i.experience == experience && i.englishKnowledge == englishKnowledge)
+                {
+                    i.GetTeachersInfo();
+                    notFound = false;
+                }
+            }
+            if (notFound) Console.WriteLine("\n\tNot Found");
+        }
+        static public void ChooseTeacher(string discipline, bool englishKnowledge)
+        {
+            Console.WriteLine("\nResult: ");
+
+            bool notFound = true;
+            foreach (Teachers i in listOfTeachers)
+            {
+                if (i.discipline == discipline && i.englishKnowledge == englishKnowledge)
+                {
+                    i.GetTeachersInfo();
+                    notFound = false;
+                }
+            }
+            if (notFound) Console.WriteLine("\n\tNot Found");
+        }
+        static public void ChooseTeacher(int age)
+        {
+            Console.WriteLine("\nResult: ");
+
+            bool notFound = true;
+            foreach (Teachers i in listOfTeachers)
+            {
+                if (i.age == age)
+                {
+                    i.GetTeachersInfo();
+                    notFound = false;
+                }
+            }
+            if (notFound) Console.WriteLine("\n\tNot Found");
+        }
+        static public void ChooseTeacher(double experience)
+        {
+            Console.WriteLine("\nResult: ");
+
+            bool notFound = true;
+            foreach (Teachers i in listOfTeachers)
+            {
+                if (i.experience == experience)
+                {
+                    i.GetTeachersInfo();
+                    notFound = false;
+                }
+            }
+            if (notFound) Console.WriteLine("\n\tNot Found");
+        }
+        static public void ChooseTeacher(string discipline)
+        {
+            Console.WriteLine("\nResult: ");
+
+            bool notFound = true;
+            foreach (Teachers i in listOfTeachers)
+            {
+                if (i.discipline == discipline)
+                {
+                    i.GetTeachersInfo();
+                    notFound = false;
+                }
+            }
+            if (notFound) Console.WriteLine("\n\tNot Found");
+        }
+        static public void ChooseTeacher(bool englishKnowledge)
+        {
+            Console.WriteLine("\nResult: ");
+
+            bool notFound = true;
+            foreach (Teachers i in listOfTeachers)
+            {
+                if (i.englishKnowledge == englishKnowledge)
+                {
+                    i.GetTeachersInfo();
+                    notFound = false;
+                }
+            }
+            if (notFound) Console.WriteLine("\n\tNot Found");
+        }
+    }
+}
